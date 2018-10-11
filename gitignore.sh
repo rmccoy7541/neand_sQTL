266a2aabd13c7a21def8d522c4c44c3de69b4a3a
Arta Seyedian <arta.seyedian@gmail.com>
2018-10-11 16:13:42 -0400

    finally fixed the gitignore problem

--- gitignore.sh
+++ gitignore.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # This is a shell script that updates the gitignore file with all of the files in the repo tthat exceed 100MB
 
-find ./* -size +100M | cat >> .gitignore
+find ./* -size +100M | cut -c 2-|  cat >> .gitignore
