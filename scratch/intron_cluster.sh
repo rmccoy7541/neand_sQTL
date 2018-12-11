27f476656af9a9e7b76490dbd02298a5dd453319
arta <arta.seyedian@gmail.com>
2018-12-11 16:48:27 -0500

    +1
    minor changes

--- scratch/intron_cluster.sh
+++ scratch/intron_cluster.sh
@@ -7,8 +7,8 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --array=1-10
 
-ml python
+ml python/2.7-anaconda
 
 python ../clustering/leafcutter_cluster.py -j ${SLURM_ARRAY_TASK_ID}.split -r intronclustering/ -m 50 -o testNE_sQTL -l 500000
 
-rm ${SLURM_ARRAY_TASK_ID}.split
+# rm ${SLURM_ARRAY_TASK_ID}.split
