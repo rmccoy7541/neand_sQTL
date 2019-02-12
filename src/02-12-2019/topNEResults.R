42e2ba947a62da5e26219e9260c79cc7aa3c5a6d
arta <arta.seyedian@gmail.com>
2019-02-12 15:23:52 -0500

    R analysis - provides gene info

--- src/02-12-2019/topNEResults.R
+++ src/02-12-2019/topNEResults.R
@@ -1,5 +1,6 @@
 library(data.table)
 library(tidyverse)
+library(Homo.sapiens)
 library(qqman)
 
 setwd("/scratch/groups/rmccoy22/Ne_sQTL/sra/juncfiles/intronclustering/WHLBLD")
@@ -14,5 +15,28 @@ dt <- do.call(rbind, lapply(1:100, function(x) tryCatch(fread(paste0("WHLBLD_nom
 dt[, drop := NULL]
 
 dt[is_top_variant == TRUE]
+
+gene_list <- genes(TxDb.Hsapiens.UCSC.hg19.knownGene) %>%
+  as.data.table()
+gene_list[, subjectHits := .I]
+symbols <- org.Hs.egSYMBOL %>%
+  as.data.table()
+gene_list <- gene_list[symbols, on = "gene_id", nomatch = 0]
+gene_list <- gene_list[, c("subjectHits", "symbol")]
+  
+coords_gr <- dt[, c("chrom", "pheno_start", "pheno_end")] %>%
+  mutate(., chrom = paste0("chr", chrom)) %>%
+  makeGRangesFromDataFrame
+  
+olaps <- findOverlaps(coords_gr, genes(TxDb.Hsapiens.UCSC.hg19.knownGene)) %>%
+  as.data.table()
+  
+dt[, queryHits := .I]
+  
+dt <- dt[olaps, on = "queryHits", nomatch = 0]
+
+dt <- dt[gene_list, on = "subjectHits", nomatch = 0]
+
+setorder(dt, p)
 
 qq(dt$p)
