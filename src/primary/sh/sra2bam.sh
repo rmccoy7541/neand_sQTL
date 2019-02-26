1719d1bf9ce52b7ada288cfab6342f5ce9d5786d
arta <arta.seyedian@gmail.com>
2019-02-26 11:08:43 -0500

    cant get this thing to work for some reason

--- src/primary/sh/sra2bam.sh
+++ src/primary/sh/sra2bam.sh
@@ -8,8 +8,8 @@
 ## YOU MUST ENTER ARRAY RANGE
 #SBATCH --array=1-212%100
 
-line=`sed "${SLURM_ARRAY_TASK_ID}q;d" ${sraListPath}sralist.txt`
+line=`sed "${SLURM_ARRAY_TASK_ID}q;d" ${sraListPath}/sralist.txt`
 
 time {
-	${homeDir}progs/sra-tools/bin/sam-dump $line | ${homeDir}progs/samtools-1.9/samtools view --threads 23 -bS - > ${line}.bam
+	${homeDir}/progs/sra-tools/bin/sam-dump $line | ${homeDir}/progs/samtools-1.9/samtools view --threads 23 -bS - > ${line}.bam
 }
