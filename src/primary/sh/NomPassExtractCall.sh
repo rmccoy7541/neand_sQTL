b43871cab8f7c4e601d8762ae3fd503390fe29bf
arta <arta.seyedian@gmail.com>
2019-02-26 12:43:42 -0500

    some node error

--- src/primary/sh/NomPassExtractCall.sh
+++ src/primary/sh/NomPassExtractCall.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=shared
 #SBATCH --job-name=NomPassExtractCall
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --array=1-100%50
 #SBATCH --mem=10GB
