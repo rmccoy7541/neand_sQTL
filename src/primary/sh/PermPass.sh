55b5aa9458ae2ccac33d08b21243230d238c9f48
arta <arta.seyedian@gmail.com>
2019-04-25 19:49:52 -0400

    whatever

--- src/primary/sh/PermPass.sh
+++ src/primary/sh/PermPass.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=shared
 #SBATCH --job-name=PermPass
 #SBATCH --nodes=1
-#SBATCH --time=4:0:0
+#SBATCH --time=10:0:0
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=5
 
@@ -13,9 +13,11 @@
 
 /home-1/aseyedi2@jhu.edu/work/progs/QTLtools/QTLtools_1.1_Ubuntu14.04_x86_64 cis \
   --vcf  $VCF \
-  --bed "$pheno" \
-  --cov  "${covariates}" \
+  --bed LUNG/LUNG.pheno.bed.gz \
+  --cov  "LUNG/Lung.v7.covariates_output.txt" \
   --permute 1000  \
-  --chunk ${SLURM_ARRAY_TASK_ID} 100 \
-  --out "${tissue}_permutations_chunk_${SLURM_ARRAY_TASK_ID}.txt" \
+  --chunk 19 100 \
+  --out "LUNG/LUNG_permutations_chunk_19.txt" \
   --normal
+  
+  sbatch -a 19 --export=VCF=$VCF,pheno=$(echo LUNG/LUNG.pheno.bed.gz),tissue=$(echo LUNG/LUNG),covariates=$(echo LUNG/Lung.v7.covariates_output.txt),abb=$(echo LUNG),full=$(echo Lung) ${scripts}/sh/PermPass.sh
