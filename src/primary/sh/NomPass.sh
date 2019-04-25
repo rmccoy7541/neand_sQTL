5517a4ea8df8489d1b31090ab6ce7225e7ca7d0a
arta <arta.seyedian@gmail.com>
2019-04-25 00:00:43 -0400

    changed some things

--- src/primary/sh/NomPass.sh
+++ src/primary/sh/NomPass.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=shared
 #SBATCH --job-name=NomPass
 #SBATCH --nodes=1
-#SBATCH --time=0:30:0
+#SBATCH --time=8:0:0
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=3
 #SBATCH --array=1-100
