d77e92f8cece3f92661d3f2fdd62f1bc5583a492
arta <arta.seyedian@gmail.com>
2019-04-26 16:29:39 -0400

    Update QTLtools-int.sh

--- src/AWS/QTLtools-int.sh
+++ src/AWS/QTLtools-int.sh
@@ -7,40 +7,35 @@
 #SBATCH --cpus-per-task=4
 
 
-line=`sed "${SLURM_ARRAY_TASK_ID}q;d" GTExCovKey.csv`
-full=$(echo $line | awk -F',' '{print $1}')
-abb=$(echo $line | awk -F',' '{print $2}')
-if grep "$abb" tissuesused.txt; then
-	# catting noms
-	for i in {1..100}; do
-	cat $abb/${abb}_nominals_chunk_${i}.txt | gzip -c >> $abb/$abb.nominals.all.chunks.txt.gz
-	done
+# catting noms
+for i in {1..100}; do
+cat $abb/${abb}_nominals_chunk_${i}.txt | gzip -c >> $abb/$abb.nominals.all.chunks.txt.gz
+done
 
-	# getting chunks
-	# ls $abb/${abb}_* | sort -V >> $abb/${abb}_chunks_list.txt
-	#######
+# getting chunks
+# ls $abb/${abb}_* | sort -V >> $abb/${abb}_chunks_list.txt
+#######
 
-	for i in {1..100}; do
-	cat ${abb}/${abb}_nominals_chunk_${i}_out.txt | gzip -c >> ${abb}/$abb.nominals.all.chunks.NE_only.txt.gz
-	done
+for i in {1..100}; do
+cat ${abb}/${abb}_nominals_chunk_${i}_out.txt | gzip -c >> ${abb}/$abb.nominals.all.chunks.NE_only.txt.gz
+done
 
-	mkdir $abb/nominals; mv $abb/*_nominals_* $abb/nominals/
+mkdir $abb/nominals; mv $abb/*_nominals_* $abb/nominals/
 
-	#######
+#######
 
-	for i in {1..100}; do
-	  cat $abb/${abb}_permutations_chunk_$i.txt | gzip -c >> $abb/${abb}.permutations_full.txt.gz
-	done
+for i in {1..100}; do
+  cat $abb/${abb}_permutations_chunk_$i.txt | gzip -c >> $abb/${abb}.permutations_full.txt.gz
+done
 
-	mkdir ${abb}/permutations; mv ${abb}/*_permutations_* ${abb}/permutations/
+mkdir ${abb}/permutations; mv ${abb}/*_permutations_* ${abb}/permutations/
 
-	#######
+#######
 
-	# for i in {1..100}; do
-	  # cat $abb/${abb}_conditionals_chunk_$i.txt | gzip -c >> $abb/${abb}.conditional_full.txt.gz
-	# done
+# for i in {1..100}; do
+  # cat $abb/${abb}_conditionals_chunk_$i.txt | gzip -c >> $abb/${abb}.conditional_full.txt.gz
+# done
 
-	# mkdir ${abb}/conditionals; mv ${abb}/*_conditionals_* ${abb}conditionals/
+# mkdir ${abb}/conditionals; mv ${abb}/*_conditionals_* ${abb}conditionals/
 
-   	mkdir /home-1/aseyedi2@jhu.edu/work/aseyedi2/sQTL/$abb
-fi
+mkdir /home-1/aseyedi2@jhu.edu/work/aseyedi2/sQTL/$abb
