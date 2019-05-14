fd48ad7869a00be079ac00523d08008bed8ce04c
Unknown <arta.seyedian@gmail.com>
2019-05-14 16:54:56 -0400

    i don't know

--- src/primary/sh/NomPass.sh
+++ src/primary/sh/NomPass.sh
@@ -25,15 +25,15 @@ junc=$(echo '/scratch/groups/rmccoy22/Ne_sQTL/sra/sqtl_junc')
 # leafcutter directory here
 leafCutter=$(echo /scratch/groups/rmccoy22/aseyedi2/leafcutter)
 
-#if [ ! -f ${abb}/${tissue}_nominals_chunk_${SLURM_ARRAY_TASK_ID}.txt ]; then
-/scratch/groups/rmccoy22/progs/QTLtools/QTLtools_1.1_Ubuntu14.04_x86_64 cis \
-    --vcf  $VCF \
-    --bed "$pheno" \
-    --cov  "$covariates" \
-    --nominal 1  \
-    --chunk $SLURM_ARRAY_TASK_ID 100 \
-    --out "${tissue}_nominals_chunk_${SLURM_ARRAY_TASK_ID}.txt"
-#fi
+if [ ! -f ${tissue}_nominals_chunk_${SLURM_ARRAY_TASK_ID}.txt ]; then
+    /scratch/groups/rmccoy22/progs/QTLtools/QTLtools_1.1_Ubuntu14.04_x86_64 cis \
+        --vcf  $VCF \
+        --bed "$pheno" \
+        --cov  "$covariates" \
+        --nominal 1  \
+        --chunk $SLURM_ARRAY_TASK_ID 100 \
+        --out "${tissue}_nominals_chunk_${SLURM_ARRAY_TASK_ID}.txt"
+fi
 
 sbatch -a $SLURM_ARRAY_TASK_ID --export=listPath=$PWD/$abb,tissue=$(echo $abb),scripts=$scripts ${scripts}sh/NomPassExtractCall.sh
 
