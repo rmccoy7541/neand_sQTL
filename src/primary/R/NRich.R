e238886339562b32cf8dec7132bfded49988caa0
Unknown <arta.seyedian@gmail.com>
2019-07-11 11:29:41 -0400

    cmdArgs

--- src/primary/R/NRich.R
+++ src/primary/R/NRich.R
@@ -3,12 +3,16 @@ library(tidyverse)
 library(qvalue)
 library(Matching)
 
-af <- fread(file = "~/Downloads/GTExWGS.AF.txt", 
+
+# 1 is the VCF filtered for allele freq, 2 is nom file, 3 is perm pass, 4 is sprime
+cmdArgs = commandArgs(trailingOnly=TRUE)
+
+af <- fread(file = cmdArgs[1], 
             colClasses = c("character", "integer", "character", "character", "numeric"))
 
 af[, AF := as.numeric(AF)]
 
-sprime <- fread("~/Desktop/sQTL/sprime_calls.txt.gz")
+sprime <- fread(cmdArgs[4])
 
 af[, variant_id := paste(CHROM, POS, REF, ALT, "b37", sep = "_")]
 sprime[, variant_id := paste(CHROM, POS, REF, ALT, "b37", sep = "_")]
@@ -21,7 +25,7 @@ neand_snps <- sprime[is_neand == TRUE & !(grepl(",", variant_id))]$variant_id
 
 af[, is_neand := variant_id %in% neand_snps]
 
-perm <- fread("~/Desktop/sQTL/ADPSBQ_permutations.txt") %>%
+perm <- fread(cmdArgs[3]) %>%
   setnames(., c("intron_cluster", "chrom", "pheno_start", "pheno_end", 
                 "strand", "total_cis", "distance", "variant_id", "variant_chrom", 
                 "var_start", "var_end", "df", "dummy", "param_1", "param_2",
