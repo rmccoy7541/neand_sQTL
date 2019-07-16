ebdf090761c95df1a21509f49bf29b41348b4de2
Unknown <arta.seyedian@gmail.com>
2019-07-16 11:05:08 -0400

    I had the wrong directory for the permutations lol

--- src/primary/sh/CallNRich.sh
+++ src/primary/sh/CallNRich.sh
@@ -21,7 +21,7 @@ Rscript /work-zfs/rmccoy22/aseyedi2/neanderthal-sqtl/src/primary/R/NRich.R \
   "/work-zfs/rmccoy22/aseyedi2/GTExWGS_VCF/GTExWGS.AF.all.txt" \
   "/work-zfs/rmccoy22/rmccoy22/sqtl/intron_clustering/tryagain/" \
   "/work-zfs/rmccoy22/aseyedi2/neanderthal-sqtl/analysis/SPRIME/sprime_calls.txt" \
-  "/work-zfs/rmccoy22/aseyedi2/GTExWGS_VCF/${tissue}_permutations.txt" \
+  "/work-zfs/rmccoy22/aseyedi2/sqtl_permutation_backup/${tissue}_permutations.txt" \
   "/work-zfs/rmccoy22/aseyedi2/sqtl_permutation_backup/sqtl_nrich/${tissue}_enrichment.txt"
 
 date
