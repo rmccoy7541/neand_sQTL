659e5b4f11635fe2f4026386b75486862e3baf90
arta <arta.seyedian@gmail.com>
2019-08-01 16:24:01 -0400

    minor

--- filter_vcf.sh
+++ filter_vcf.sh
@@ -5,7 +5,7 @@
 scripts=$(echo /home-1/aseyedi2@jhu.edu/work/aseyedi2/neand_sQTL/src/primary/)
 
 # filter the non-biallelic sites from genotype file using bcftools; see script for details
-sbatch --wait ${scripts}/sh/bcf_tools.sh /scratch/groups/rmccoy22/Ne_sQTL/files
+sbatch --wait ${scripts}/sh/bcftools_filter.sh /scratch/groups/rmccoy22/Ne_sQTL/files
 # index our friend with tabix
 echo "Indexing our friend..."
 interact -p shared -t 360:00 -c 6
