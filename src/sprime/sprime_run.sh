b3391eae2015982a61f79c57cb8168fa50347ef5
arta <arta.seyedian@gmail.com>
2019-09-30 16:07:57 -0400

    added some test code

--- src/sprime/sprime_run.sh
+++ src/sprime/sprime_run.sh
@@ -38,3 +38,12 @@ java -Xmx12g -jar ~/work/progs/sprime.jar \
   outgroup=yri.txt \
   map=genetic_map/plink.all_autosomes.GRCh38.map \
   out=output/results.chr${i}
+
+
+# java -Xmx12g -jar ~/work/progs/sprime.jar \
+#   gt=/scratch/groups/rmccoy22/aseyedi2/sQTLv8/sprime/merged_filtered_all_autosomes.vcf.gz \
+#   outgroup=/scratch/groups/rmccoy22/aseyedi2/sQTLv8/sprime/yri.txt \
+#   map=/scratch/groups/rmccoy22/aseyedi2/sQTLv8/sprime/genetic_map/plink.all_autosomes.GRCh38.map \
+#   out=/scratch/groups/rmccoy22/aseyedi2/sQTLv8/sprime_test/results.test.ADAMSTL3.minscore \
+#   chrom=chr15:83537709-84537709 \
+#   minscore=50000
