afcb5af42d34955dcb073cde28c1e422f304656b
arta <arta.seyedian@gmail.com>
2019-11-12 15:56:36 -0500

    still can't find anything

--- src/analysis/merge_tables.R
+++ src/analysis/merge_tables.R
@@ -1,12 +1,19 @@
 library(tidyr)
 library(dplyr)
+library(data.table)
 
-introns <- read.table("../intron_counts/GTEx_v8_junctions_nohead.gct.gz",stringsAsFactors=FALSE,header=TRUE)
-sqtl <- read.table("~/work/aseyedi2/sQTLv8/data/GTEx_Analysis_v8_sQTL/Testis.v8.sqtl_signifpairs.txt.gz",stringsAsFactors=FALSE,header=TRUE)
-vcf <- read.table("../vcf/vcf_for_merge.txt",stringsAsFactors=FALSE, row.names = NULL)
+introns <- fread("../intron_counts/GTEx_v8_junctions_nohead.gct.gz",stringsAsFactors=FALSE,header=TRUE)
+vcf <- fread("../vcf/vcf_for_merge.txt",stringsAsFactors=FALSE)
+tissue_names <-c("Adipose_Subcutaneous", "Adipose_Visceral_Omentum", "Adrenal_Gland", "Artery_Aorta", "Artery_Coronary", "Artery_Tibial", "Brain_Amygdala", "Brain_Anterior_cingulate_cortex_BA24", "Brain_Caudate_basal_ganglia", "Brain_Cerebellar_Hemisphere", "Brain_Cerebellum", "Brain_Cortex", "Brain_Frontal_Cortex_BA9", "Brain_Hippocampus", "Brain_Hypothalamus", "Brain_Nucleus_accumbens_basal_ganglia", "Brain_Putamen_basal_ganglia", "Brain_Spinal_cord_cervical_c-1", "Brain_Substantia_nigra", "Breast_Mammary_Tissue", "Cells_Cultured_fibroblasts", "Cells_EBV-transformed_lymphocytes", "Colon_Sigmoid", "Colon_Transverse", "Esophagus_Gastroesophageal_Junction", "Esophagus_Mucosa", "Esophagus_Muscularis", "Heart_Atrial_Appendage", "Heart_Left_Ventricle", "Kidney_Cortex", "Liver", "Lung", "Minor_Salivary_Gland", "Muscle_Skeletal", "Nerve_Tibial", "Ovary", "Pancreas", "Pituitary", "Prostate", "Skin_Not_Sun_Exposed_Suprapubic", "Skin_Sun_Exposed_Lower_leg", "Small_Intestine_Terminal_Ileum", "Spleen", "Stomach", "Testis", "Thyroid", "Uterus", "Vagina", "Whole_Blood")
+
+#get the paths for all sqtl perm pass results per tissue
+tissue_names <- paste0("~/work/aseyedi2/sQTLv8/data/GTEx_Analysis_v8_sQTL/", tissue_names, ".v8.sqtl_signifpairs.txt.gz")
+
+#read them all in
+sqtl_by_tissue <- do.call(rbind, lapply(tissue_names, function(x) sqtl <- fread(x)))
 
 # change intron cluster names to match names in sQTL file
-introns2 <- as.data.frame(sapply(introns, gsub, pattern="_", replacement=":"))
+introns2 <- as.data.table(sapply(introns, gsub, pattern="_", replacement=":"))
 
 # separate sQTL intron cluster ID field to match with IDs from introns file
 sqtl2 <- separate(sqtl, phenotype_id, c("cluster_pos","cluster_id"), sep=":clu_", remove=TRUE)
