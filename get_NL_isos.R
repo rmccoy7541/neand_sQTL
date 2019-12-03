e22332948bc1aeb4ec01a827a487d1e68c084791
artaHome <arta.seyedian@gmail.com>
2019-12-03 09:13:52 -0500

    save

--- get_NL_isos.R
+++ get_NL_isos.R
@@ -3,6 +3,7 @@ library(data.table)
 
 dt <- fread("Combined2_Ovary.txt")
 dt1 <- dt[,17:197]
+dt2 <- dt$Name
 dt2 <- cbind(dt2, dt[,198:ncol(dt)])
 
 dt1 <- as.data.table(dt1 %>% pivot_longer(-Name, names_to = "tissue_id", values_to = "counts"))
@@ -11,4 +12,6 @@ dt2 <- as.data.table(dt2 %>% pivot_longer(-Name, names_to = "individual", values
 
 dt1$individual <- gsub("^([^.]*.[^.]*)..*$", "\\1", dt1$tissue_id)
 
-final <- dplyr::full_join(dt1, dt2, by = c("Name", "individual"))
+final <- as.data.table(dplyr::full_join(dt1, dt2, by = c("Name", "individual")))
+
+final <- final[is_NL == 1] 
