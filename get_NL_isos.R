ba07c3720aa7320b693708ace7eb80181eff527c
artaHome <arta.seyedian@gmail.com>
2019-12-05 11:34:33 -0500

    Condensed into merge_tables.R

--- get_NL_isos.R
+++ get_NL_isos.R
@@ -7,19 +7,19 @@ colnames(dt)[17] <- "transcript_id"
 
 list_dt <- split.default(dt, nchar(names(dt)) > 10)
 
-xcrips <- cbind(list_dt[[1]][,"variant_id"], list_dt[[1]][,"Name"], list_dt[[2]][,5:ncol(list_dt[[2]])])
+xcrips <- cbind(list_dt[[1]][,"variant_id"], list_dt[[2]][,"transcript_id"], list_dt[[2]][,5:ncol(list_dt[[2]])])
 
-nl_iso <- cbind(list_dt[[1]][,"variant_id"], list_dt[[1]][,"Name"], list_dt[[1]][,13:ncol(list_dt[[1]])])
-
-#add variant id
-xcrips <- as.data.table(xcrips %>% pivot_longer(-c(Name, variant_id), names_to = "tissue_id", values_to = "counts"))
+nl_iso <- cbind(list_dt[[1]][,"variant_id"], list_dt[[2]][,"transcript_id"], list_dt[[1]][,13:ncol(list_dt[[1]])])
 
 #add variant id
-nl_iso <- as.data.table(nl_iso %>% pivot_longer(-c(Name, variant_id), names_to = "individual", values_to = "is_NL"))
+xcrips <- as.data.table(xcrips %>% pivot_longer(-c(transcript_id, variant_id), names_to = "tissue_id", values_to = "counts"))
+
+#add variant id
+nl_iso <- as.data.table(nl_iso %>% pivot_longer(-c(transcript_id, variant_id), names_to = "individual", values_to = "is_NL"))
 
 xcrips$individual <- gsub("^([^.]*.[^.]*)..*$", "\\1", xcrips$tissue_id)
 
-final <- as.data.table(dplyr::full_join(xcrips, nl_iso, by = c("Name", "individual", "variant_id")))
+final <- as.data.table(dplyr::full_join(xcrips, nl_iso, by = c("transcript_id", "individual", "variant_id")))
 
 #final <- na.omit(final[is_NL == 1])
 
--- src/analysis/merge_tables.R
+++ src/analysis/merge_tables.R
@@ -1,69 +1,53 @@
+### USAGE ###
+# Rscript table_merge_SY.R <intron counts file> <sQTL file> <tissue_id>
 library(tidyr)
 library(dplyr)
 library(data.table)
 
-introns <-
-  read.table(
-    "../intron_counts/GTEx_v8_junctions_nohead.gct.gz",
-    stringsAsFactors = FALSE,
-    header = TRUE
-  )
+# command line input specifies which intron counts and sQTL file to read
+# and also the tissue name
+args = commandArgs(trailingOnly=TRUE)
 
-vcf <-
-  read.table("../vcf/vcf_for_merge.txt",
-             stringsAsFactors = FALSE,
-             header = TRUE)
+# read in intron counts file for one specific tissue
+introns <- fread(args[2],stringsAsFactors=FALSE,header=TRUE)
 
-#tissue_names <-c("Adipose_Subcutaneous", "Adipose_Visceral_Omentum", "Adrenal_Gland", "Artery_Aorta", "Artery_Coronary", "Artery_Tibial", "Brain_Amygdala", "Brain_Anterior_cingulate_cortex_BA24", "Brain_Caudate_basal_ganglia", "Brain_Cerebellar_Hemisphere", "Brain_Cerebellum", "Brain_Cortex", "Brain_Frontal_Cortex_BA9", "Brain_Hippocampus", "Brain_Hypothalamus", "Brain_Nucleus_accumbens_basal_ganglia", "Brain_Putamen_basal_ganglia", "Brain_Spinal_cord_cervical_c-1", "Brain_Substantia_nigra", "Breast_Mammary_Tissue", "Cells_Cultured_fibroblasts", "Cells_EBV-transformed_lymphocytes", "Colon_Sigmoid", "Colon_Transverse", "Esophagus_Gastroesophageal_Junction", "Esophagus_Mucosa", "Esophagus_Muscularis", "Heart_Atrial_Appendage", "Heart_Left_Ventricle", "Kidney_Cortex", "Liver", "Lung", "Minor_Salivary_Gland", "Muscle_Skeletal", "Nerve_Tibial", "Ovary", "Pancreas", "Pituitary", "Prostate", "Skin_Not_Sun_Exposed_Suprapubic", "Skin_Sun_Exposed_Lower_leg", "Small_Intestine_Terminal_Ileum", "Spleen", "Stomach", "Testis", "Thyroid", "Uterus", "Vagina", "Whole_Blood")
+# read in sQTL file for one specific tissue
+sqtl <- fread(args[3], stringsAsFactors=FALSE, header=TRUE)
 
-#get the paths for all sqtl perm pass results per tissue
-#tissue_names <- paste0("~/work/aseyedi2/sQTLv8/data/GTEx_Analysis_v8_sQTL/", tissue_names, ".v8.sqtl_signifpairs.txt.gz")
+# separate intron cluster field to get ENSEMBL ID
+sqtl_sep <- separate(sqtl, phenotype_id, c("chrom","start","end","cluster_id","ENSEMBL_ID"), sep=":", remove=TRUE)
 
-# read in one sQTL file
-sqtl <-
-  read.table(
-    "~/work/aseyedi2/sQTLv8/data/GTEx_Analysis_v8_sQTL/Ovary.v8.sqtl_signifpairs.txt.gz",
-    stringsAsFactors = FALSE,
-    header = TRUE
-  )
+# read in VCF file
+vcf <- fread("../vcf/vcf_for_merge.txt.gz", stringsAsFactors=FALSE, header=TRUE)
 
-sqtl_sep <-
-  separate(
-    sqtl,
-    phenotype_id,
-    c("chrom", "start", "end", "cluster_id", "ENSEMBL_ID"),
-    sep = ":",
-    remove = TRUE
-  )
+tissue_name <- args[1]
 
-combined <-
-  inner_join(sqtl_sep, introns, by = c("ENSEMBL_ID" = "Description"))
+dt <- inner_join(inner_join(sqtl_sep, introns, by=c("ENSEMBL_ID"="Description")), vcf, by=c("variant_id"="ID"))
 
-write.table(combined,
-            file = "combined_SY.txt",
+### Get NL Isoforms
+
+colnames(dt)[17] <- "transcript_id"
+
+list_dt <- split.default(dt, nchar(names(dt)) > 10)
+
+xcrips <- cbind(list_dt[[1]][,"variant_id"], list_dt[[2]][,"transcript_id"], list_dt[[2]][,5:ncol(list_dt[[2]])])
+
+nl_iso <- cbind(list_dt[[1]][,"variant_id"], list_dt[[2]][,"transcript_id"], list_dt[[1]][,13:ncol(list_dt[[1]])])
+
+#add variant id
+xcrips <- as.data.table(xcrips %>% pivot_longer(-c(transcript_id, variant_id), names_to = "tissue_id", values_to = "counts"))
+
+#add variant id
+nl_iso <- as.data.table(nl_iso %>% pivot_longer(-c(transcript_id, variant_id), names_to = "individual", values_to = "is_NL"))
+
+xcrips$individual <- gsub("^([^.]*.[^.]*)..*$", "\\1", xcrips$tissue_id)
+
+final <- as.data.table(dplyr::full_join(xcrips, nl_iso, by = c("transcript_id", "individual", "variant_id")))
+
+#final <- na.omit(final[is_NL == 1])
+
+write.table(final,
+            file = paste0(tissue_name, "_NL_isos.txt"),
             sep = "\t",
+            row.names = F,
             quote = FALSE)
-
-# join intron-sqtl and vcf dataframes by variant ID
-# if there are multiple intron clusters that correspond to one variant,
-# duplicate the line from the vcf
-
-combined2 <- inner_join(combined, vcf, by = c("variant_id" = "ID"))
-
-test <- as.data.table(subset(combined2, select = c(variant_id, Name)))
-
-lapply(strsplit(names(combined2)), "\\."), "[[", 1))
-
-do.call(rbind, Filter(function(x) length(x)==11, ips.info))
-
-# to do this, for each row (i.e variant/cluster combo), I want to take the binary value for whether each individual (e.g. GTEX.111YS) has the Neanderthal 
-# allele at that SNP, followed by a semicolon and then the sum total of intron counts found in that individual in one tissue/sample (e.g GTEX.111YS.1126.SM.5GZYQ).
-# This must be done for all samples.
-
-write.table(combined2,
-            file = "combined2_SY.txt",
-            sep = "\t",
-            quote = FALSE)
-
-# combine identical sample columns from vcf and intron files
-# want to end up with a single sample column with info: GT;intron read count
