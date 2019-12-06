2f7948457b74c456afa3c5f29654ca2ce36aa4ae
arta <arta.seyedian@gmail.com>
2019-12-06 11:49:29 -0500

    fixed merge_tables some more

--- src/analysis/merge_tables.R
+++ src/analysis/merge_tables.R
@@ -9,10 +9,12 @@ library(data.table)
 args = commandArgs(trailingOnly=TRUE)
 
 # read in intron counts file for one specific tissue
-introns <- fread(args[2],stringsAsFactors=FALSE,header=TRUE)
+introns <- fread(args[1],stringsAsFactors=FALSE,header=TRUE)
+# introns <- fread("../splitIC/Ovary_intronCounts.txt",stringsAsFactors=FALSE,header=TRUE)
 
 # read in sQTL file for one specific tissue
-sqtl <- fread(args[3], stringsAsFactors=FALSE, header=TRUE)
+sqtl <- fread(args[2], stringsAsFactors=FALSE, header=TRUE)
+# sqtl <- fread("/scratch/groups/rmccoy22/aseyedi2/sQTLv8/data/GTEx_Analysis_v8_sQTL/Ovary.v8.sqtl_signifpairs.txt.gz", stringsAsFactors=FALSE, header=TRUE)
 
 # separate intron cluster field to get ENSEMBL ID
 sqtl_sep <- separate(sqtl, phenotype_id, c("chrom","start","end","cluster_id","ENSEMBL_ID"), sep=":", remove=TRUE)
@@ -20,7 +22,12 @@ sqtl_sep <- separate(sqtl, phenotype_id, c("chrom","start","end","cluster_id","E
 # read in VCF file
 vcf <- fread("../vcf/vcf_for_merge.txt.gz", stringsAsFactors=FALSE, header=TRUE)
 
-tissue_name <- args[1]
+# INTRONS_FILE=../splitIC/Ovary_intronCounts.txt 
+# SQTL_FILE=/scratch/groups/rmccoy22/aseyedi2/sQTLv8/data/GTEx_Analysis_v8_sQTL/Ovary.v8.sqtl_signifpairs.txt.gz
+# TISSUE=`sed "36q;d" tissues.txt`
+
+# tissue_name <- args[3]
+tissue_name <- "Ovary"
 
 dt <- inner_join(inner_join(sqtl_sep, introns, by=c("ENSEMBL_ID"="Description")), vcf, by=c("variant_id"="ID"))
 
@@ -30,9 +37,14 @@ colnames(dt)[17] <- "transcript_id"
 
 list_dt <- split.default(dt, nchar(names(dt)) > 10)
 
-xcrips <- cbind(list_dt[[1]][,"variant_id"], list_dt[[2]][,"transcript_id"], list_dt[[2]][,5:ncol(list_dt[[2]])])
+variant_id <- as.character(list_dt[[1]][,"variant_id"])
+transcript_id <- as.character(list_dt[[2]][,"transcript_id"])
 
-nl_iso <- cbind(list_dt[[1]][,"variant_id"], list_dt[[2]][,"transcript_id"], list_dt[[1]][,13:ncol(list_dt[[1]])])
+xcrips <- data.table(variant_id, list_dt[[2]][,5:ncol(list_dt[[2]])])
+
+nl_iso <- data.table(variant_id, transcript_id, list_dt[[1]][,13:ncol(list_dt[[1]])])
+
+rm(variant_id, transcript_id)
 
 #add variant id
 xcrips <- as.data.table(xcrips %>% pivot_longer(-c(transcript_id, variant_id), names_to = "tissue_id", values_to = "counts"))
