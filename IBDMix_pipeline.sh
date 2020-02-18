a278432f4c38ed7ccadb9ab2edaca186d4a278ed
arta <arta.seyedian@gmail.com>
2020-02-18 15:22:57 -0500

    Changes to IBDMix Pipeline

--- IBDMix_pipeline.sh
+++ IBDMix_pipeline.sh
@@ -8,7 +8,8 @@ for i in {1..22}; do
   gunzip AltaiNea.hg19_1000g.${i}.mod.vcf.gz
 done
 
-vcf-concat -f concatList.txt > AltaiNea.hg19_100g.concat.vcf
+#don't need to concat
+vcf-concat -f concatList.txt > AltaiNea.hg19_1000g.concat.vcf
 
 # now liftover the vcf
 wget http://hgdownload.cse.ucsc.edu/goldenpath/hg19/liftOver/hg19ToHg38.over.chain.gz
@@ -28,4 +29,5 @@ java -jar ~/work/progs/gatk-4.0.12.0/gatk-package-4.0.12.0-local.jar LiftoverVcf
 bcftools view -m 2 -M 2 -v snps --threads 23 -O v -o GTExWGSGenotypeMatrixBiallelicOnly_v8.vcf.gz /scratch/groups/rmccoy22/aseyedi2/sQTLv8/data/vcf/GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_838Indiv_Analysis_Freeze.vcf.gz
 
 # From IBDMix
+# Try taking the first MB of chromosome 1 and see if that works with generate_gt
 generate_gt -a AltaiNea.hg38_1000g.concat.LowQualRemoved.vcf -m GTExWGSGenotypeMatrixBiallelicOnly_v8.vcf -o IBDMix_GT.txt
