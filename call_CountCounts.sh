2fb15081414fbb3e79128fbb4e5c34edf8ff09ad
artaHome <arta.seyedian@gmail.com>
2020-02-25 17:12:49 -0500

    figured out how to count Counts

---
+++ call_CountCounts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=CountCounts
+#SBATCH --time=2:0:0
+#SBATCH --partition=shared
+#SBATCH --nodes=1
+# number of tasks (processes) per node
+#SBATCH --array=1-49
+#######################################
+# This is to call the script that counts the number of transcript/variant ID pairs
+# with 0 counts for each genotype and with more than 0 counts for each genotype
+
+ml R/3.5.1
+ml gcc
+
+line=`sed "${SLURM_ARRAY_TASK_ID}q;d" fileList.txt`
+
+Rscript countCounts.R $line $line
